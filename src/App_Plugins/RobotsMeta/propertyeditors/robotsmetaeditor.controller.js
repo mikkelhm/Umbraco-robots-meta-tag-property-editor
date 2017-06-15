54b3642f7685771ce31a5135a02a9ffa4c5b903c
Søren Kottal <snielsen43@gmail.com>
2017-06-15 13:50:20 +0200

    Create robotsmetaeditor.controller.js

--- src/App_Plugins/RobotsMeta/propertyeditors/robotsmetaeditor.controller.js
+++ src/App_Plugins/RobotsMeta/propertyeditors/robotsmetaeditor.controller.js
@@ -2,16 +2,22 @@
     function ($scope) {
         $scope.listValues = [
             {
+                dictionaryKeyForName: "robotsMetaTag_indexFollowName",
+                dictionaryKeyForDescription: "robotsMetaTag_indexFollowDescription",
                 name: "Index this, and follow links",
                 description: "Will allow indexing of this page, and allow the search engine to follow links",
                 value: "index,follow,noodp"
             },
             {
+                dictionaryKeyForName: "robotsMetaTag_noindexFollowName",
+                dictionaryKeyForDescription: "robotsMetaTag_noindexFollowDescription",
                 name: "Dont index this, but follow links",
                 description: "Will disallow indexing of this page, but will allow the search engine to follow links",
                 value: "noindex,follow,noodp"
             },
             {
+                dictionaryKeyForName: "robotsMetaTag_noindexNofollowName",
+                dictionaryKeyForDescription: "robotsMetaTag_noindexNofollowDescription",
                 name: "Index none",
                 description: "Will disallow indexing of this page, and disallow it to follow links",
                 value: "noindex,nofollow,noodp"
